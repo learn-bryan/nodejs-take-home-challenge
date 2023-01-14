bef5476dcef3bc355c6940d9021244213e97053b
learn-bryan <bryan@bryanlearn.com>
2023-01-14 18:34:36 -0500

    Implement column layout configuration

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const Sequelize = require('sequelize')
+const {Vehicle} = require("./models/vehicle.model.js")
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 
 // Create database connection
-const sequelize = new Sequelize(process.env.DB_CONNECTION_URL)
+const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
 
 // Authenticate with the database
 sequelize.authenticate().then(() => {
@@ -14,6 +17,18 @@ sequelize.authenticate().then(() => {
 
 const app = express();
 const PORT = 3000;
+
+const lookupColumnConfig = async (provider_name) => {
+    try {
+        const columnConfigPath = path.join(__dirname, process.env.COLUMN_CONFIG);
+        const config = JSON.parse(fs.readFileSync(columnConfigPath, 'utf8'));
+        return config[provider_name];
+    } catch (error) {
+        throw new Error(`Error reading the config file: ${error}`)
+    }
+}
+
+console.log(lookupColumnConfig("provider_all"))
 
 app.listen(PORT, (error) => {
     if (!error) console.log(`Server started on port ${PORT}`)
